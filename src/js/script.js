c874ccbdd79e7a81654784f2010fbeea9eba7fdf
Lyutik13 <lyutikov.viktor.96@mail.ru>
2022-11-03 08:57:02 +0300

    plagin_slick

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".carousel__inner").slick({
+        speed: 1200,
+        prevArrow:
+            '<button type="button" class="slick-prev"><img src="img/slaid/left.svg"></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img src="img/slaid/right.svg"></button>',
+    });
+});
