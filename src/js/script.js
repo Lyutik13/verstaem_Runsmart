dbb5686a0212bf7b50e2174679668fdacc765521
Lyutik13 <lyutikov.viktor.96@mail.ru>
2022-11-04 15:49:00 +0300

    Tiny_Slider_2_carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,27 @@
-$(document).ready(function () {
-    $(".carousel__inner").slick({
-        speed: 1200,
-        prevArrow:
-            '<button type="button" class="slick-prev"><img src="img/slaid/left.svg"></button>',
-        nextArrow:
-            '<button type="button" class="slick-next"><img src="img/slaid/right.svg"></button>',
-    });
+// $(document).ready(function () {
+//     $(".carousel__inner").slick({
+//         speed: 1200,
+//         prevArrow:
+//             '<button type="button" class="slick-prev"><img src="img/slaid/left.svg"></button>',
+//         nextArrow:
+//             '<button type="button" class="slick-next"><img src="img/slaid/right.svg"></button>',
+//     });
+// });
+
+// Tiny Slider 2
+const slider = tns({
+    container: ".carousel__inner",
+    items: 1,
+    slideBy: "page",
+    autoplay: false,
+    controls: false,
+    nav: false,
+});
+
+document.querySelector(".prev").addEventListener("click", function () {
+    slider.goTo("prev");
+});
+
+document.querySelector(".next").addEventListener("click", function () {
+    slider.goTo("next");
 });
