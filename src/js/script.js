f9f2fa6261898eca67efa5103099ec8c4b36ad40
Lyutik13 <lyutikov.viktor.96@mail.ru>
2022-11-08 16:18:10 +0300

    carousel_end

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,51 @@
-// $(document).ready(function () {
-//     $(".carousel__inner").slick({
-//         speed: 1200,
-//         prevArrow:
-//             '<button type="button" class="slick-prev"><img src="img/slaid/left.svg"></button>',
-//         nextArrow:
-//             '<button type="button" class="slick-next"><img src="img/slaid/right.svg"></button>',
-//     });
-// });
+$(document).ready(function () {
+    $(".carousel__inner").slick({
+        speed: 1200,
+        prevArrow:
+            '<button type="button" class="slick-prev"><img src="img/slaid/left.svg"></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img src="img/slaid/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                },
+            },
+        ],
+    });
 
-// Tiny Slider 2
-const slider = tns({
-    container: ".carousel__inner",
-    items: 1,
-    slideBy: "page",
-    autoplay: false,
-    controls: false,
-    nav: false,
-});
+    $("ul.catalog__tabs").on(
+        "click",
+        "li:not(.catalog__tab_active)",
+        function () {
+            $(this)
+                .addClass("catalog__tab_active")
+                .siblings()
+                .removeClass("catalog__tab_active")
+                .closest("div.container")
+                .find("div.catalog__content")
+                .removeClass("catalog__content_active")
+                .eq($(this).index())
+                .addClass("catalog__content_active");
+        }
+    );
 
-document.querySelector(".prev").addEventListener("click", function () {
-    slider.goTo("prev");
-});
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on("click", function (e) {
+                e.preventDefault();
+                $(".catalog-item__content")
+                    .eq(i)
+                    .toggleClass("catalog-item__content_active");
+                $(".catalog-item__list")
+                    .eq(i)
+                    .toggleClass("catalog-item__list_active");
+            });
+        });
+    }
 
-document.querySelector(".next").addEventListener("click", function () {
-    slider.goTo("next");
+    toggleSlide(".catalog-item__link");
+    toggleSlide(".catalog-item__back");
 });
