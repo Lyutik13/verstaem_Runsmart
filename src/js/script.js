d44e7a3da2fc2e5cbfaf51e9a4a79dbb68cf4fd9
Lyutik13 <lyutikov.viktor.96@mail.ru>
2022-11-08 18:54:12 +0300

    media

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
     $(".carousel__inner").slick({
         speed: 1200,
+        autoplay: true,
+        autoplaySpeed: 2000,
         prevArrow:
             '<button type="button" class="slick-prev"><img src="img/slaid/left.svg"></button>',
         nextArrow:
             '<button type="button" class="slick-next"><img src="img/slaid/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 991,
                 settings: {
-                    dots: true,
+                    // dots: true,
                     arrows: false,
                 },
             },
