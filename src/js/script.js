ed3159b42feecd3fcd3c876d15d6a91addada59b
Lyutik13 <lyutikov.viktor.96@mail.ru>
2022-11-11 03:04:39 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,70 @@
 $(document).ready(function () {
-    $(".carousel__inner").slick({
-        speed: 1200,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow:
-            '<button type="button" class="slick-prev"><img src="img/slaid/left.svg"></button>',
-        nextArrow:
-            '<button type="button" class="slick-next"><img src="img/slaid/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    // dots: true,
-                    arrows: false,
-                },
-            },
-        ],
-    });
+  $(".carousel__inner").slick({
+    speed: 1200,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="img/slaid/left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="img/slaid/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          // dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
 
-    $("ul.catalog__tabs").on(
-        "click",
-        "li:not(.catalog__tab_active)",
-        function () {
-            $(this)
-                .addClass("catalog__tab_active")
-                .siblings()
-                .removeClass("catalog__tab_active")
-                .closest("div.container")
-                .find("div.catalog__content")
-                .removeClass("catalog__content_active")
-                .eq($(this).index())
-                .addClass("catalog__content_active");
-        }
-    );
-
-    function toggleSlide(item) {
-        $(item).each(function (i) {
-            $(this).on("click", function (e) {
-                e.preventDefault();
-                $(".catalog-item__content")
-                    .eq(i)
-                    .toggleClass("catalog-item__content_active");
-                $(".catalog-item__list")
-                    .eq(i)
-                    .toggleClass("catalog-item__list_active");
-            });
-        });
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
     }
+  );
 
-    toggleSlide(".catalog-item__link");
-    toggleSlide(".catalog-item__back");
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+
+  // Modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+  });
+  $(".button_mini").on("click", function () {
+    $(".overlay, #order").fadeIn("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
 });
