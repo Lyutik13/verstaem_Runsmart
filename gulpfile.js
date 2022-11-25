f537555628c193e798a08297aea0617c85daa746
Lyutik13 <lyutikov.viktor.96@mail.ru>
2022-11-25 23:50:34 +0300

    gulp_set

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync");
 const sass = require("gulp-sass")(require("sass"));
-const rename = require("gulp-rename");
-const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
 
-// Static server
 gulp.task("server", function () {
-    browserSync.init({
-        server: {
-            baseDir: "src",
-        },
-    });
+  browserSync({
+    server: {
+      baseDir: "src",
+    },
+  });
+
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task("styles", function () {
-    return gulp
-        .src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-        .pipe(rename({prefix: "", suffix: ".min",}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({ compatibility: "ie8" }))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+  return gulp
+    .src("src/sass/**/*.+(scss|sass)")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(rename({ suffix: ".min", prefix: "" }))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function () {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
 });
 
 gulp.task("default", gulp.parallel("watch", "server", "styles"));
